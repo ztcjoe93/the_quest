e28ccd0db8902053f9a868f396e869e4cf788a94
joe <ztcjoe93@gmail.com>
2021-11-07 20:19:10 +0800

    added tile movement, updated db data access

--- internal/area/area.go
+++ internal/area/area.go
@@ -1,8 +1,155 @@
 package area
 
+import (
+	"fmt"
+	"log"
+)
+
+type Position struct {
+	X, Y int
+}
+
 type Grid struct {
-	areaCode string
-	matrix   [][]Tile
+	areaCode                string
+	matrix                  [][]Tile
+	startingPos, currentPos Position
+}
+
+func CreateGrid(tiles []*Tile, maxX int, maxY int, pos Position) Grid {
+	if len(tiles) == 0 {
+		log.Fatal("No tiles provided in slice.")
+	}
+
+	matrix := make([][]Tile, maxY)
+	for row := range matrix {
+		matrix[row] = make([]Tile, maxX)
+	}
+
+	for _, tile := range tiles {
+		matrix[tile.coordinates.Y][tile.coordinates.X] = *tile
+	}
+
+	return Grid{
+		areaCode:    tiles[0].areaCode,
+		matrix:      matrix,
+		startingPos: Position{pos.X, pos.Y},
+		currentPos:  Position{pos.X, pos.Y},
+	}
+}
+
+func (grid *Grid) GetGridData() {
+	for ind, row := range grid.matrix {
+		fmt.Println("Currently at row ", ind)
+		for colind, col := range row {
+			if col.areaCode == "" {
+				fmt.Printf("%v,%v\n", ind, colind)
+			} else {
+				fmt.Printf("%v\n", col.coordinates)
+			}
+		}
+	}
+}
+
+func (grid *Grid) GetCurrentPos() {
+	fmt.Printf("Current position: (%v,%v)\n", grid.currentPos.X, grid.currentPos.Y)
+}
+
+func (grid *Grid) MoveToTile(tile Tile) {
+	grid.currentPos = tile.coordinates
+}
+
+/*
+	Get accessible tiles based on the current position that the player is on the
+	grid.
+
+	Algorithm works where (on a grid of hexagon tiles):
+		- tiles on even x-axis can access adjacent tiles with same/-1 y-axis
+		- tiles on odd x-axis can access adjacent tiles with same/+1 y-axis
+
+	Why hexagons? Because hexagons are the *_bestagons_*
+*/
+func (grid *Grid) GetAccessibleTiles() []Tile {
+	fmt.Println(grid.currentPos)
+	curPosEven := grid.currentPos.X%2 == 0
+
+	accessible := make([]Tile, 0)
+	x := grid.currentPos.X
+	y := grid.currentPos.Y
+
+	if curPosEven {
+		// accounting for left-side edge case
+		if x > 0 {
+			if grid.matrix[y][x-1].areaCode != "" {
+				accessible = append(accessible, grid.matrix[y][x-1])
+			}
+
+			// accounting for top-edge case
+			if y > 0 {
+				if grid.matrix[y-1][x-1].areaCode != "" {
+					accessible = append(accessible, grid.matrix[y-1][x-1])
+				}
+			}
+		}
+
+		// accounting for right-side edge case
+		if x+1 < len(grid.matrix[y]) {
+			if grid.matrix[y][x+1].areaCode != "" {
+				accessible = append(accessible, grid.matrix[y][x+1])
+			}
+
+			// accounting for top-edge case
+			if y > 0 {
+				if grid.matrix[y-1][x+1].areaCode != "" {
+					accessible = append(accessible, grid.matrix[y-1][x+1])
+				}
+			}
+		}
+	} else {
+		if grid.matrix[y][x-1].areaCode != "" {
+			accessible = append(accessible, grid.matrix[y][x-1])
+		}
+
+		// accounting for bottom-side edge case
+		if y+1 != len(grid.matrix) {
+			if grid.matrix[y+1][x-1].areaCode != "" {
+				accessible = append(accessible, grid.matrix[y+1][x-1])
+			}
+		}
+
+		// accounting for right-side edge case
+		if x+1 < len(grid.matrix[y]) {
+			if grid.matrix[y][x+1].areaCode != "" {
+				accessible = append(accessible, grid.matrix[y][x+1])
+			}
+
+			if y+1 != len(grid.matrix) {
+				if grid.matrix[y+1][x+1].areaCode != "" {
+					accessible = append(accessible, grid.matrix[y+1][x+1])
+				}
+			}
+		}
+	}
+
+	// rest of the common checks for top-y and bottom-y cases
+	if y > 0 {
+		if grid.matrix[y-1][x].areaCode != "" {
+			accessible = append(accessible, grid.matrix[y-1][x])
+		}
+	}
+	if y+1 < len(grid.matrix) {
+		if grid.matrix[y+1][x].areaCode != "" {
+			accessible = append(accessible, grid.matrix[y+1][x])
+		}
+	}
+
+	fmt.Printf("%d tile(s) to move to: \n", len(accessible))
+	/*
+		for _, tile := range accessible {
+			fmt.Printf("%v,", tile.coordinates)
+		}
+	*/
+
+	return accessible
 }
 
 /*
@@ -18,8 +165,7 @@ type Tile struct {
 	content          string
 	areaCode         string
 	monsterEncounter bool
-	x                int
-	y                int
+	coordinates      Position
 }
 
 /*
@@ -32,7 +178,10 @@ func CreateTile(content string, areaCode string, monsterEncounter bool,
 		content:          content,
 		areaCode:         areaCode,
 		monsterEncounter: monsterEncounter,
-		x:                x,
-		y:                y,
+		coordinates:      Position{x, y},
 	}
+}
+
+func (tile *Tile) GetCoordinates() Position {
+	return tile.coordinates
 }
